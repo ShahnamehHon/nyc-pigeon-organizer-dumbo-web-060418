12edb3375710575a7b4118661fd7f520b3608b55
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-20 17:02:57 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |data, names|
       names.each do |name|
-          pigeons[name] ||= {}
-          pigeons[name][key] ||= []
+          pigeons[name] ?= {}
+          pigeons[name][key] ?= []
           pigeons[name][key] << data.to_s
         end
   end
